5a1f11ddd1c0125c39710974916e0a791cb39d3c
nhsoft.lsd <nhsoft.lsd@hotmail.com>
2026-03-19 14:07:25 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+// App 配置
+window.APP_CONFIG = {
+    // App Store 下载地址
+    appStoreUrl: 'https://apps.apple.com/us/app/image-to-pdf-pdf-conveter/id6760633814',
+    // 隐私协议地址
+    privacyPolicyUrl: 'https://docs.google.com/document/d/169eQTJhcbCk-_lkmRRpwrjiYkh6ZfgxmkyBIuksduec/edit?usp=sharing',
+    // 联系邮箱
+    contactEmail: 'nhsoft.lsd@gmail.com',
+    // App 名称
+    appName: 'Image to PDF'
+};
